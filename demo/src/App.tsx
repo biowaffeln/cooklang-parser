ec6f6fa8692def910347ae6f2a63b1da7f6f932b
Mark Kvetny <m.kvetny@outlook.com>
2022-01-21 15:07:11 +0100

    style: formatting

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -5,55 +5,55 @@ import { RecipeRenderer } from "./components/RecipeRenderer";
 import GithubCorner from "react-github-corner";
 
 export const App = () => {
-  const [recipeText, setRecipeText] = useState(sampleRecipe);
-  const [recipeName, setRecipeName] = useState(sampleRecipeTitle);
+	const [recipeText, setRecipeText] = useState(sampleRecipe);
+	const [recipeName, setRecipeName] = useState(sampleRecipeTitle);
 
-  const data = Recipe(recipeText);
+	const data = Recipe(recipeText);
 
-  return (
-    <div className="container px-4 mx-auto">
-      <GithubCorner href="https://github.com/biowaffeln/cooklang-parser" />
-      <h1 className="mt-16 text-3xl font-bold">Cooklang Parser Demo</h1>
-      <div className="grid gap-6 mt-10 md:grid-cols-2">
-        <div className="flex flex-col h-[70vh]">
-          <input
-            className="h-10 mb-2 border border-gray-300 rounded bg-gray-50
+	return (
+		<div className="container px-4 mx-auto">
+			<GithubCorner href="https://github.com/biowaffeln/cooklang-parser" />
+			<h1 className="mt-16 text-3xl font-bold">Cooklang Parser Demo</h1>
+			<div className="grid gap-6 mt-10 md:grid-cols-2">
+				<div className="flex flex-col h-[70vh]">
+					<input
+						className="h-10 mb-2 border border-gray-300 rounded bg-gray-50
                        focus:outline-none focus:border-gray-400 px-4 py-2 text-lg"
-            placeholder="Recipe Name"
-            value={recipeName}
-            onChange={(e) => setRecipeName(e.target.value)}
-          />
-          <textarea
-            className="rounded border p-4 border-gray-300 bg-gray-50
+						placeholder="Recipe Name"
+						value={recipeName}
+						onChange={(e) => setRecipeName(e.target.value)}
+					/>
+					<textarea
+						className="rounded border p-4 border-gray-300 bg-gray-50
                        text-lg text-gray-800 h-full resize-none
                        focus:outline-none focus:border-gray-400"
-            value={recipeText}
-            onChange={(e) => setRecipeText(e.target.value)}
-          />
-        </div>
-        <Tabs.Root defaultValue="Output" className="max-h-[70vh] flex flex-col">
-          <Tabs.List className="-mb-px flex gap-1">
-            <Tabs.Trigger value="Output" className="tab">
-              Output
-            </Tabs.Trigger>
-            <Tabs.Trigger value="AST" className="tab">
-              AST
-            </Tabs.Trigger>
-          </Tabs.List>
-          <div className="overflow-scroll border border-gray-300">
-            <Tabs.TabsContent value="AST">
-              <pre className="p-4 text-sm whitespace-pre-wrap text-gray-700">
-                {JSON.stringify(data, null, 2)}
-              </pre>
-            </Tabs.TabsContent>
-            <Tabs.Content value="Output">
-              <RecipeRenderer name={recipeName} recipe={data}></RecipeRenderer>
-            </Tabs.Content>
-          </div>
-        </Tabs.Root>
-      </div>
-    </div>
-  );
+						value={recipeText}
+						onChange={(e) => setRecipeText(e.target.value)}
+					/>
+				</div>
+				<Tabs.Root defaultValue="Output" className="max-h-[70vh] flex flex-col">
+					<Tabs.List className="-mb-px flex gap-1">
+						<Tabs.Trigger value="Output" className="tab">
+							Output
+						</Tabs.Trigger>
+						<Tabs.Trigger value="AST" className="tab">
+							AST
+						</Tabs.Trigger>
+					</Tabs.List>
+					<div className="overflow-scroll border border-gray-300">
+						<Tabs.TabsContent value="AST">
+							<pre className="p-4 text-sm whitespace-pre-wrap text-gray-700">
+								{JSON.stringify(data, null, 2)}
+							</pre>
+						</Tabs.TabsContent>
+						<Tabs.Content value="Output">
+							<RecipeRenderer name={recipeName} recipe={data}></RecipeRenderer>
+						</Tabs.Content>
+					</div>
+				</Tabs.Root>
+			</div>
+		</div>
+	);
 };
 
 const sampleRecipe = `>> source: https://www.dinneratthezoo.com/wprm_print/6796
--- demo/src/components/RecipeRenderer.tsx
+++ demo/src/components/RecipeRenderer.tsx
@@ -2,120 +2,120 @@ import { VFC } from "react";
 import type { Recipe } from "@cooklang/parser";
 
 interface RecipeRendererProps {
-  name: string;
-  recipe: ReturnType<typeof Recipe>;
+	name: string;
+	recipe: ReturnType<typeof Recipe>;
 }
 
 export const RecipeRenderer: VFC<RecipeRendererProps> = ({ name, recipe }) => {
-  return (
-    <div className="p-4">
-      <h2 className="text-3xl font-semibold mt-2">{name}</h2>
-      <section className="mt-4 overflow-x-auto">
-        <table className="table-fixed">
-          <tbody>
-            {Object.entries(recipe.metadata as Record<string, string>).map(
-              ([key, value]) => (
-                <tr key={key}>
-                  <td className="whitespace-nowrap font-medium pr-4 align-top">
-                    {key}:
-                  </td>
-                  <td className="break-all align-top">
-                    {value?.startsWith("http") ? (
-                      <a
-                        href={value}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-blue-500 underline"
-                      >
-                        {value}
-                      </a>
-                    ) : (
-                      value
-                    )}
-                  </td>
-                </tr>
-              )
-            )}
-          </tbody>
-        </table>
-      </section>
-      <div className="flex flex-wrap mt-8 gap-4">
-        <section className="p-4 border border-zinc-300 rounded min-w-[10rem] w-full md:w-auto relative">
-          <h3
-            className="text-xs font-bold text-zinc-700 uppercase bg-white px-3
+	return (
+		<div className="p-4">
+			<h2 className="text-3xl font-semibold mt-2">{name}</h2>
+			<section className="mt-4 overflow-x-auto">
+				<table className="table-fixed">
+					<tbody>
+						{Object.entries(recipe.metadata as Record<string, string>).map(
+							([key, value]) => (
+								<tr key={key}>
+									<td className="whitespace-nowrap font-medium pr-4 align-top">
+										{key}:
+									</td>
+									<td className="break-all align-top">
+										{value?.startsWith("http") ? (
+											<a
+												href={value}
+												target="_blank"
+												rel="noopener noreferrer"
+												className="text-blue-500 underline"
+											>
+												{value}
+											</a>
+										) : (
+											value
+										)}
+									</td>
+								</tr>
+							)
+						)}
+					</tbody>
+				</table>
+			</section>
+			<div className="flex flex-wrap mt-8 gap-4">
+				<section className="p-4 border border-zinc-300 rounded min-w-[10rem] w-full md:w-auto relative">
+					<h3
+						className="text-xs font-bold text-zinc-700 uppercase bg-white px-3
                        inline-block absolute -top-2 left-[5px]"
-          >
-            Ingredients
-          </h3>
-          <ul className="mt-2 list-disc list-inside space-y-0.5">
-            {recipe.ingredients.map((ingredient) => (
-              <li key={ingredient.name} className="pr-2">
-                <span className="font-medium text-rose-700">
-                  {ingredient.quantity} {ingredient.units}
-                </span>{" "}
-                <span>{ingredient.name}</span>
-              </li>
-            ))}
-          </ul>
-        </section>
-        <section className="p-4 border border-zinc-300 rounded min-w-[10rem] w-full md:w-auto relative">
-          <h3
-            className="text-xs font-bold text-zinc-700 uppercase bg-white px-3
+					>
+						Ingredients
+					</h3>
+					<ul className="mt-2 list-disc list-inside space-y-0.5">
+						{recipe.ingredients.map((ingredient) => (
+							<li key={ingredient.name} className="pr-2">
+								<span className="font-medium text-rose-700">
+									{ingredient.quantity} {ingredient.units}
+								</span>{" "}
+								<span>{ingredient.name}</span>
+							</li>
+						))}
+					</ul>
+				</section>
+				<section className="p-4 border border-zinc-300 rounded min-w-[10rem] w-full md:w-auto relative">
+					<h3
+						className="text-xs font-bold text-zinc-700 uppercase bg-white px-3
                        inline-block absolute -top-2 left-[5px]"
-          >
-            Cookware
-          </h3>
-          <ul className="mt-2 list-inside list-disc">
-            {recipe.cookware.map((cookware) => (
-              <li key={cookware.name}>
-                <span>{cookware.quantity}</span>
-                <span>{cookware.name}</span>
-              </li>
-            ))}
-          </ul>
-        </section>
-      </div>
-      <ol className="list-decimal ml-6 mt-10 space-y-4">
-        {recipe.steps.map((step, i) => (
-          <li key={i} className="text-lg leading-relaxed">
-            {step.map((token, i) => {
-              if (token.type === "text") {
-                return (
-                  <span key={i} data-token="text">
-                    {token.value}
-                  </span>
-                );
-              }
-              if (token.type === "timer") {
-                return (
-                  <span key={i} data-token="timer" title={token.name}>
-                    {token.quantity} {token.units}
-                  </span>
-                );
-              }
-              if (token.type === "ingredient") {
-                return (
-                  <span
-                    key={i}
-                    data-token="ingredient"
-                    title={`${token.quantity} ${token.units}`}
-                  >
-                    {token.name}
-                  </span>
-                );
-              }
-              if (token.type === "cookware") {
-                return (
-                  <span key={i} data-token="cookware">
-                    {token.name}
-                  </span>
-                );
-              }
-            })}
-          </li>
-        ))}
-      </ol>
-      <div className="h-20" />
-    </div>
-  );
+					>
+						Cookware
+					</h3>
+					<ul className="mt-2 list-inside list-disc">
+						{recipe.cookware.map((cookware) => (
+							<li key={cookware.name}>
+								<span>{cookware.quantity}</span>
+								<span>{cookware.name}</span>
+							</li>
+						))}
+					</ul>
+				</section>
+			</div>
+			<ol className="list-decimal ml-6 mt-10 space-y-4">
+				{recipe.steps.map((step, i) => (
+					<li key={i} className="text-lg leading-relaxed">
+						{step.map((token, i) => {
+							if (token.type === "text") {
+								return (
+									<span key={i} data-token="text">
+										{token.value}
+									</span>
+								);
+							}
+							if (token.type === "timer") {
+								return (
+									<span key={i} data-token="timer" title={token.name}>
+										{token.quantity} {token.units}
+									</span>
+								);
+							}
+							if (token.type === "ingredient") {
+								return (
+									<span
+										key={i}
+										data-token="ingredient"
+										title={`${token.quantity} ${token.units}`}
+									>
+										{token.name}
+									</span>
+								);
+							}
+							if (token.type === "cookware") {
+								return (
+									<span key={i} data-token="cookware">
+										{token.name}
+									</span>
+								);
+							}
+						})}
+					</li>
+				))}
+			</ol>
+			<div className="h-20" />
+		</div>
+	);
 };
