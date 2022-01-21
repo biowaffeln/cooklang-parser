a4dd77ffa9cf21d5e0bbc4468e0d5fca2a61c592
Mark Kvetny <m.kvetny@outlook.com>
2022-01-21 14:35:47 +0100

    feat(demo): deploy demo to github pages

---
+++ demo/deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+pnpm build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:biowaffeln/cooklang-parser.git main:gh-pages
+
+cd -
